0d61903d38e7d16f891d0950a85275d76f92a0cc
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-18 10:23:02 -0700

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace RestShopTest
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Payroll Problem using RestShop API");
+        }
+    }
+}
